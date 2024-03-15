9f9f8f19107d44392cd0dd3a6168a549e3c72054
diegoinost02 <diegoinostroza00@gmail.com>
2024-03-15 19:57:09 -0300

    Se agrega nav-bar

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { MoviesComponent } from './components/movies/movies.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, MoviesComponent],
+  imports: [NavBarComponent, HeaderComponent, MoviesComponent],
   templateUrl: './layout.component.html'
 })
 export class LayoutComponent {
