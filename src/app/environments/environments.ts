8707e3dcadf9406eec41bd65603839356b091968
diegoinost02 <diegoinostroza00@gmail.com>
2024-03-14 22:54:25 -0300

    Servicios para API

---
+++ src/app/environments/environments.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    API_MOVIES_URL: 'https://api.themoviedb.org/3',
+    API_TOKEN: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Y2QxZmZlYzczYmFkMDZhZTVjYzE3YmM1ZTM2ZmJlNyIsInN1YiI6IjY1MWM5MGQyMjIzYThiMDBhYjNkNzk1NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1lR71ex3VtGpR59edjtAvEmtU96NqOqvAI8B7CpQV_k'
+}
---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,51 @@
+import { Injectable, inject, signal } from '@angular/core';
+import { environment } from '../environments/environments';
+import { HttpClient } from '@angular/common/http';
+import { MoviesResponse } from '../models/interface.movie';
+import { Video } from '../models/interface.video';
+import { tap } from 'rxjs';
+import { FormControl } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+  private http = inject(HttpClient);
+
+  apiUrl: string = environment.API_MOVIES_URL;
+  apiToken: string = environment.API_TOKEN;
+
+  movies$ = signal<MoviesResponse | null>(null);
+  searching$ = signal<boolean>(false);
+  page$ = signal<number>(1);
+  // input$ = signal<FormControl>(new FormControl('', { nonNullable: true }));
+
+  getPopularMovies(page: number) {
+    return this.http.get<MoviesResponse>(`${this.apiUrl}/movie/popular?page=${page}`, {
+      headers: {
+          accept: 'application/json',
+          Authorization: `Bearer ${this.apiToken}`
+      }
+    })
+  }
+
+  searchMovie(query: string, page: number) {
+    return this.http.get<MoviesResponse>(`${this.apiUrl}/search/movie?query=${query}&page=${page}`, {
+      headers: {
+          accept: 'application/json',
+          Authorization: `Bearer ${this.apiToken}`,
+      }
+    }).pipe(tap(movies => this.movies$.update(() => movies)));
+  }
+
+  // https://www.youtube.com/embed/videoKey
+  getVideoMovie(movieId: number) {
+    return this.http.get<Video>(`${this.apiUrl}/movie/${movieId}/videos`, {
+      headers: {
+          accept: 'application/json',
+          Authorization: `Bearer ${this.apiToken}`
+      }
+    });
+  }
+}
